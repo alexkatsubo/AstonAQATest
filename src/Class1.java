baafc8b571c52488b293e171e4f5fe4fbe63716c
alexkatsubo <vasa.tolich@gmail.com>
2023-11-16 21:41:20 +0300

    first commit

---
+++ src/Class1.java
@@ -0,0 +1,37 @@
+public class Class1 {
+    public  static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void checkSumSign() {
+        int a = 20;
+        int b = -25;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 75;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value >= 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 3;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Class1.printThreeWords();
+        Class1.checkSumSign();
+        Class1.printColor();
+        Class1.compareNumbers();
+    }
+}
