a738316a29f3d87de0ee6ef9dca7c8d1c74d1517
alexkatsubo <vasa.tolich@gmail.com>
2023-12-28 00:35:47 +0300

    Lesson15

---
+++ src/test/java/org/example/pages/Product.java
@@ -0,0 +1,38 @@
+package org.example.pages;
+
+import java.util.List;
+
+public class Product {
+    public String name;
+    public int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static int getTotalPrice(List<Product> products) {
+        int total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name= " + name +
+                ", price=" + price +
+                "}\n";
+    }
+
+}
